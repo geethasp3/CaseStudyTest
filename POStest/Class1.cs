ea37ead722041a6499e6d0b2241db8390298e381
TPAdmin <panickergita03@gmail.com>
2018-12-18 09:10:43 +0100

    Add project files.

---
+++ POStest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POStest
+{
+    public class Class1
+    {
+    }
+}
