4fa9bb4e9294cd2026aa1fb179ee66cd399c92f9
geethasp3 <panickergita03@gmail.com>
2018-12-18 12:10:56 +0100

    test

--- POStest/LoginTest.cs
+++ POStest/LoginTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace POStest
 {
-   public class LoginTest:POSBaseClass
+    [TestFixtureSetUp]
+    public class Logintest
     {
-   
+
+
+
+
+
+        public class LoginTest1 : POSBaseClass
+        {
+            public static string abc;
+        }
     }
 }
