9565552138e5f4bc7e78a49646ac68d37a10c21e
geethasp3 <34023143+geethasp3@users.noreply.github.com>
2018-12-19 11:20:30 +0100

    Add files via upload

--- POStest/LoginTest2.cs
+++ POStest/LoginTest2.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace POStest
 {
-   public class LoginTest2:POSBaseClass
+   public class LoginTest2
     {
+        [Test]
+        public void test()
+        {
+
+            IWebDriver _driver = new ChromeDriver();
+            
+
+        }
     }
 }
